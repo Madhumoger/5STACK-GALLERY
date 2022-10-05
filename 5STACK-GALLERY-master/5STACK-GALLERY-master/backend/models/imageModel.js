6dbd2d17679991c6ff91e0f14c5a0c90f02d4650
Madhu Moger <97347207+Madhumoger@users.noreply.github.com>
2022-10-05 20:40:35 +0530

    Delete 5STACK-GALLERY-master/5STACK-GALLERY-master directory

--- 5STACK-GALLERY-master/5STACK-GALLERY-master/backend/models/imageModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const imgSchema = new mongoose.Schema({
-  category: String,
-  imgName: String,
-});
-
-module.exports = ImageModel = mongoose.model("Image", imgSchema);
